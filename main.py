91cc51b8ca438568190f786dde699f2527acbb22
María Fernanda Medina <maria.medina27@gmail.com>
2025-10-05 17:38:35 -0500

    change all

--- main.py
+++ main.py
@@ -42,9 +42,6 @@ def root():
     return {"message": "Hello World"}
 
 
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 10000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
     
     
         
